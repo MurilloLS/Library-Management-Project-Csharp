189c525dbff08302ba6f25b1e702fe0bab6bba91
Murillo Santos <murillo.lds01@gmail.com>
2024-08-10 18:32:02 -0300

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagement.Models
+{
+  public class Book
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Author Author { get; set; }
+  }
+}
