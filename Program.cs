1ec504785239708081bee6598b73026e6433c324
Murillo Santos <murillo.lds01@gmail.com>
2024-08-13 15:53:55 -0300

    Removed lists, optimized code to use only dictionaries and queue and add loan methods

--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ namespace LibraryManagement
       LibraryService libraryService = new LibraryService();
 
       // Criar autores
-      Author author1 = new Author("J.K. Rowling");
-      Author author2 = new Author("George Orwell");
+      Author author1 = new Author("Murillo");
+      Author author2 = new Author("Matheus");
 
       // Adicionar autores
       libraryService.AddAuthor(author1);
       libraryService.AddAuthor(author2);
 
       // Criar livros
-      Book book1 = new Book("Harry Potter and the Sorcerer's Stone", author1);
-      Book book2 = new Book("1984", author2);
+      Book book1 = new Book("Star Wars - Capitulo I", author1);
+      Book book2 = new Book("Starfild", author2);
 
       // Adicionar livros
       libraryService.AddBook(book1);
@@ -29,59 +29,25 @@ namespace LibraryManagement
       Console.WriteLine("All Books:");
       libraryService.GetBooks();
 
-      // Listar todos os autores
-      Console.WriteLine("\nAll Authors:");
-      libraryService.GetAuthors();
-
-      // Buscar livro pelo título
-      Console.WriteLine("\nSearch for a book by title:");
-      string searchTitle = Console.ReadLine();
-      Book foundBook = libraryService.SearchBookByTitle(searchTitle);
-      if (foundBook != null)
-      {
-        Console.WriteLine($"Found Book by Title: {foundBook.Title}, Author: {foundBook.Author.Name}");
-      }
-
-      // Buscar livro pelo ID
-      Console.WriteLine("\nSearch for a book by ID:");
+      // Buscar e pedir empréstimo de um livro pelo ID
+      Console.WriteLine("\nRequest a book loan by ID:");
       string searchBookIdInput = Console.ReadLine();
       if (Guid.TryParse(searchBookIdInput, out Guid searchBookId))
       {
-        Book foundBookById = libraryService.SearchBookById(searchBookId);
-        if (foundBookById != null)
-        {
-          Console.WriteLine($"Found Book by ID: {foundBookById.Title}, Author: {foundBookById.Author.Name}");
-        }
+        libraryService.RequestBookLoan(searchBookId);
       }
       else
       {
         Console.WriteLine("Invalid ID format.");
       }
 
-      // Buscar autor pelo nome
-      Console.WriteLine("\nSearch for an author by name:");
-      string searchAuthor = Console.ReadLine();
-      Author foundAuthor = libraryService.SearchAuthorByName(searchAuthor);
-      if (foundAuthor != null)
-      {
-        Console.WriteLine($"Found Author by Name: {foundAuthor.Name}");
-      }
+      // Processar a solicitação de empréstimo
+      Console.WriteLine("\nProcessing loan request:");
+      libraryService.ProcessLoanRequest();
 
-      // Buscar autor pelo ID
-      Console.WriteLine("\nSearch for an author by ID:");
-      string searchAuthorIdInput = Console.ReadLine();
-      if (Guid.TryParse(searchAuthorIdInput, out Guid searchAuthorId))
-      {
-        Author foundAuthorById = libraryService.SearchAuthorById(searchAuthorId);
-        if (foundAuthorById != null)
-        {
-          Console.WriteLine($"Found Author by ID: {foundAuthorById.Name}");
-        }
-      }
-      else
-      {
-        Console.WriteLine("Invalid ID format.");
-      }
+      // Tentar processar novamente para mostrar a fila vazia
+      Console.WriteLine("\nProcessing another loan request:");
+      libraryService.ProcessLoanRequest();
     }
   }
 }
--- Services/LibraryService.cs
+++ Services/LibraryService.cs
@@ -4,51 +4,75 @@ namespace LibraryManagement.Services
 {
   public class LibraryService
   {
-    private List<Author> authors;
-    private List<Book> books;
+
     private Dictionary<string, Book> bookByTitleDictionary;
     private Dictionary<Guid, Book> bookByIdDictionary;
     private Dictionary<string, Author> authorByNameDictionary;
     private Dictionary<Guid, Author> authorByIdDictionary;
+    private Queue<Book> loanQueue;
 
     public LibraryService()
     {
-      authors = new List<Author>();
-      books = new List<Book>();
       bookByTitleDictionary = new Dictionary<string, Book>();
       bookByIdDictionary = new Dictionary<Guid, Book>();
       authorByNameDictionary = new Dictionary<string, Author>();
       authorByIdDictionary = new Dictionary<Guid, Author>();
-
+      loanQueue = new Queue<Book>();
     }
     public void AddAuthor(Author author)
     {
-      authors.Add(author);
-
-      if (authorByNameDictionary.TryAdd(author.Name, author))
+      if (!authorByNameDictionary.ContainsKey(author.Name))
       {
+        authorByNameDictionary.Add(author.Name, author);
         authorByIdDictionary.Add(author.Id, author);
       }
       else
       {
-        Console.WriteLine("NotFound!");
+        Console.WriteLine("Book already exists in the dictionary.");
       }
 
     }
     public void AddBook(Book book)
     {
-      books.Add(book);
-
-      if (bookByTitleDictionary.TryAdd(book.Title, book))
+      if (!bookByTitleDictionary.ContainsKey(book.Title))
       {
+        bookByTitleDictionary.Add(book.Title, book);
         bookByIdDictionary.Add(book.Id, book);
       }
       else
-      {
-        Console.WriteLine("NotFound!");
+      {   
+        Console.WriteLine("Book already exists in the dictionary.");
       }
 
     }
+
+    public void RequestBookLoan(Guid bookId) //Classe para requerir emprestimo do livro
+    {
+      if(bookByIdDictionary.TryGetValue(bookId, out Book book))
+      {
+        loanQueue.Enqueue(book);
+        System.Console.WriteLine($"Book '{book.Title}' has been added to the loan queue.");
+      }
+      else
+      {
+        System.Console.WriteLine("Book not found.");
+      }
+    }
+
+    public void ProcessLoanRequest() //Classe para processar o emprestimo
+    {
+      if (loanQueue.Count > 0)
+      {
+        Book loanedBook = loanQueue.Dequeue();
+        System.Console.WriteLine($"Book '{loanedBook.Title}' has been loaned out.");
+      }
+      else 
+      {
+        Console.WriteLine("No loan request in the queue.");
+      }
+    }
+
+
 
     public Book SearchBookByTitle(string title)
     {
